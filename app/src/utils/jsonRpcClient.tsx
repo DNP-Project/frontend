4320813ff8aa35b2f52aadd2b06e58854b85d76e
aequor-cntrl <53840534+aequor-cntrl@users.noreply.github.com>
2025-04-27 18:54:30 +0300

    Correct the axios baseURL

--- app/src/utils/jsonRpcClient.tsx
+++ app/src/utils/jsonRpcClient.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://stage.dnp-project.ru/backend/rpc",
+  baseURL: "https://stage.dnp-project.ru/rpc",
   headers: {
     "Content-Type": "application/json",
   },
