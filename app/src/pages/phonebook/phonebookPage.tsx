bb90463453d64f4920cdcc20783366278f8e208f
aequor-cntrl <53840534+aequor-cntrl@users.noreply.github.com>
2025-04-29 15:24:57 +0300

    Change error handling protocol

--- app/src/pages/phonebook/phonebookPage.tsx
+++ app/src/pages/phonebook/phonebookPage.tsx
@@ -1,8 +1,13 @@
 import { useEffect, useState } from "react";
+import {
+  Alert,
+  AlertDescription,
+  AlertTitle,
+} from "@/components/ui/alert"
 import { EntryCard } from "@/components/phonebook/EntryCard";
 import { AddEntry } from "@/components/phonebook/AddEntry";
 import { fetchEntries, addEntry, editEntry, deleteEntry } from "@/services/phonebookService";
-import { ErrorBoundary } from "@/components/phonebook/ErrorBoundary"
+import { Terminal } from "lucide-react";
 
 export function PhonebookPage() {
   const [entries, setEntries] = useState<{
@@ -20,17 +25,28 @@ export function PhonebookPage() {
   }, []);
 
   const handleAddEntry = async (name: string, phone: string, email: string) => {
-    try {
-      const newEntry = await addEntry(name, phone, email);
+    if (!name || !phone || !email) {
+      setError("All fields are required.");
+      return;
+    }
+    if (!/^\+\d{11,15}$/.test(phone)) {
+      setError("Phone number must be written in the format +XXXXXXXXXXX");
+      return;
+    }
+    const newEntry = await addEntry(name, phone, email);
       setEntries((prev) => [...prev, newEntry]);
       setError(null);
-    } catch (error: any) {
-      console.error("Failed to add entry:", error.response?.data || error.message);
-      setError(error.response?.data?.error?.message || "An error occurred while adding the entry.");
-    }
   };
 
   const handleEditEntry = async (id: string, name: string, phone: string, email: string) => {
+    if (!name || !phone || !email) {
+      setError("All fields are required.");
+      return;
+    }
+    if (!/^\+\d{11,15}$/.test(phone)) {
+      setError("Phone number must be written in the format +XXXXXXXXXXX");
+      return;
+    }
     const updatedEntry = await editEntry(id, name, phone, email);
     setEntries((prev) =>
       prev.map((entry) => (entry.id === id ? updatedEntry : entry))
@@ -43,13 +59,18 @@ export function PhonebookPage() {
   };
 
   return (
-    <ErrorBoundary>
-      <div className="container mx-auto p-4 fixed top-0 left-0 w-full" style={{ backgroundColor: "var(--color-primary-foreground)" }}>
+    <div className="container mx-auto p-4 fixed top-0 left-0 w-full" style={{ backgroundColor: "var(--color-primary-foreground)" }}>
       <div className="fixed top-0 left-0 w-full flex justify-between pl-5 pr-3 items-center backdrop-blur-md" style={{ backgroundColor: "rgba(var(--sidebar-primary), 0.5)" }}>
         <h1 className="text-2xl text-center font-bold mb-1">My Phonebook</h1>
         <AddEntry onAdd={handleAddEntry} />
       </div>
-      {error && <div className="error-message">{error}</div>}
+      {error && 
+      <Alert>
+        <Terminal className="h-4 w-4" />
+        <AlertTitle>Something went wrong</AlertTitle>
+        <AlertDescription>{error}</AlertDescription>
+      </Alert>
+      }
       <div className="flex gap-8 justify-center-safe flex-wrap scroll-mt-5 mt-12">
         {entries.map((entry) => (
           <EntryCard
@@ -63,6 +84,5 @@ export function PhonebookPage() {
         ))}
       </div>
     </div>
-    </ErrorBoundary>
   );
 }
