fa7b29d78a1d841c009e9e77f8868435354afe60
aequor-cntrl <53840534+aequor-cntrl@users.noreply.github.com>
2025-04-29 14:31:08 +0300

    Fix edit bug

--- app/src/services/phonebookService.tsx
+++ app/src/services/phonebookService.tsx
@@ -4,8 +4,8 @@ export async function addEntry(name: string, phone: string, email: string) {
   return await jsonRpcRequest("AddContact", { name, phone, email});
 }
 
-export async function editEntry(name: string, phone: string, email: string) {
-  return await jsonRpcRequest("UpdateContact", {name, phone, email});
+export async function editEntry(id: string, name: string, phone: string, email: string) {
+  return await jsonRpcRequest("UpdateContact", {id, name, phone, email});
 }
 
 export async function deleteEntry(id: string) {
